b8ce398cdb6204b48ba2b969cb072b0debcae02c
Agarwal Dipesh <dipeshagarwal2003@gmail.com>
2014-10-04 00:56:26 +0530

    working on p2

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
-
-
-
-<!doctype html>
-<html lang="en">
-    <?php include("/header.php"); ?>
-<body>
-
-<?php include("/logic.php"); ?>
-    <?php include("/content.php"); ?>
-
-
-
-
-</body>
-</html>
+<h1>Hello World Project 3!</h1>
