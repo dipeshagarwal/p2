8f87790e6fe03bcf60d86bcd5366e8813d0faf8e
Agarwal Dipesh <dipeshagarwal2003@gmail.com>
2014-10-04 00:52:02 +0530

    working on p2

--- content.php
+++ content.php
@@ -1,34 +1,21 @@
-<div class="container"> 
-    
-	<h2>xkcd Style Password Generator</h2>
-    
-    <h4>This Password Generator generates passwords consisting from common words. Such words are hard to guess (even by brute force), but easy to remember,  making them interesting password choices.</h4>
-    
-    <h3>Fill Details</h3>   
-
-	<form method="POST" action="/p2/index.php">  
-	
-    <label name="countwords">Number of words</label>
-    <input type="text" id="countwords" name="countwords" value="10"/><br /> 
-    
-    <label name="lengthpassword">Maximum Length of password</label>
-    <input type="text" id="lengthpassword" name="lengthpassword" value="6"/><br />
-      
-    <label name="uppercase">Upper case First Letter?</label>
-    <input type="checkbox" name="uppercase" value="checkbox" <?php echo ($uppercase) ?  'checked = "checked"' : ''; ?>/><br />
-             
-                
-    <label name="symbol">Use a symbol?</label>
-    <input type="checkbox" name="symbol" value="symbol" /><br />
-                        
-    <label name="number">Include a number?</label>
-    <input type="checkbox" name="number" value="number" /><br />         
-          
-    <input type="submit" name="submit" value="Generate a new  Password"/>
-    </form>
-    
-    <br />
-    <p>Your Password is:</p>
-	<p><?php echo ($password) ?  $password : ''; ?></p>
-    
+<div class="container">
+<h2>xkcd Style Password Generator</h2>
+<h4>This Password Generator generates passwords consisting from common words. Such words are hard to guess (even by brute force), but easy to remember, making them interesting password choices.</h4>
+<h3>Fill Details</h3>
+<form method="POST" action="/p2/index.php">
+<label name="countwords">Number of words</label>
+<input type="text" id="countwords" name="countwords" value="10"/><br />
+<label name="lengthpassword">Maximum Length of password</label>
+<input type="text" id="lengthpassword" name="lengthpassword" value="6"/><br />
+<label name="uppercase">Upper case First Letter?</label>
+<input type="checkbox" name="uppercase" value="checkbox" <?php echo ($uppercase) ? 'checked = "checked"' : ''; ?>/><br />
+<label name="symbol">Use a symbol?</label>
+<input type="checkbox" name="symbol" value="symbol" /><br />
+<label name="number">Include a number?</label>
+<input type="checkbox" name="number" value="number" /><br />
+<input type="submit" name="submit" value="Generate a new Password"/>
+</form>
+<br />
+<p>Your Password is:</p>
+<p><?php echo ($password) ? $password : ''; ?></p>
 </div>
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
-    Live URL of the project.  http://p2.dagarwal-dwa.me/
-	
-	Github:   https://github.com/dipeshagarwal/p2
-	
-    Description of the project:  I have developed a xkcd Style Password Generator which generates passwords consisting from common words. Such words are hard to guess (even by brute force), but easy to remember, making them interesting password choices.
 
-    
+
+Live URL of the project.  http://p2.dagarwal-dwa.me/
+
+Github:   https://github.com/dipeshagarwal/p2
+
+Description of the project:  I have developed a xkcd Style Password Generator which generates passwords consisting from common words. Such words are hard to guess (even by brute force), but easy to remember, making them interesting password choices.
+
